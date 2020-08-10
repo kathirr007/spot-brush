2b17ad01fad08117c621754eb190208a682f1a6c
kkumarasamy <kkumarasamy@edit-place.com>
2020-08-10 12:27:26 +0530

    Swap bootstrap-vue to vuetify

--- components/topNavBar.vue
+++ components/topNavBar.vue
@@ -1,31 +1,96 @@
 <template>
-    <b-navbar toggleable="lg" type="dark" variant="dark">
-        <b-navbar-brand to="/"><img src="/spotbrush-logo.svg" alt="Spotbrush"></b-navbar-brand>
+    <div>
+        <v-navigation-drawer dark temporary v-model="sideNav" class="teal" app>
+            <v-list>
+                <v-list-item v-for="item in menuItems" :key="item.title" :to="item.link">
+                    <v-list-item-action>
+                        <v-icon>mdi-{{item.icon}}</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>{{item.title}}</v-list-item-content>
+                </v-list-item>
+                <v-list-item v-if="userIsAuthenticated">
+                    <v-list-item-action>
+                        <v-icon>mdi-exit_to_app</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>Logout</v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar dark color="teal darken-4">
+            <v-app-bar-nav-icon class="d-md-none d-sm-flex" @click.stop="sideNav = !sideNav" />
+            <!-- <v-toolbar-side-icon @click.stop="sideNav = !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon> -->
+            <v-toolbar-title class="pl-md-0 pl-sm-2">
+                <nuxt-link to="/" tag="span" class="navbar-brand" style="cursor: pointer"><img src="/spotbrush-logo.svg" alt="Spotbrush"></nuxt-link>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items class="d-none d-sm-flex">
+                <v-btn text v-for="item in menuItems" :key="item.title" class="text-capitalize" :to="item.link">
+                    <v-icon left dark class="hidden-sm-and-down">{{item.icon}}</v-icon>
+                    {{item.title}}
+                </v-btn>
+                <v-btn v-if="userIsAuthenticated" class="text-capitalize" text @click="onLogout">
+                    <v-icon left dark class="hidden-sm-and-down">exit_to_app</v-icon> Logout
+                </v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+        <!-- <b-navbar toggleable="lg" type="dark" variant="dark">
+            <b-navbar-brand to="/"><img src="/spotbrush-logo.svg" alt="Spotbrush"></b-navbar-brand>
 
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-        <b-collapse id="nav-collapse" is-nav>
-            <!-- <b-navbar-nav>
-                <b-nav-item href="#">Link</b-nav-item>
-                <b-nav-item href="#" disabled>Disabled</b-nav-item>
-            </b-navbar-nav> -->
+            <b-collapse id="nav-collapse" is-nav>
 
-            <!-- Right aligned nav items -->
-            <b-navbar-nav class="ml-auto">
-                <!-- <b-nav-item to="/" class="text-primary">Home</b-nav-item>
-                <b-nav-item to="/test" class="text-primary">Login</b-nav-item> -->
-                <b-button class="mr-2" to="/">Home</b-button>
-                <b-button variant="success" class="mr-2" to="/login">Login</b-button>
-                <b-button to="/index copy">Signup</b-button>
-            </b-navbar-nav>
-        </b-collapse>
-    </b-navbar>
+                <b-navbar-nav class="ml-auto">
+                    <b-button class="mr-2" to="/">Home</b-button>
+                    <b-button variant="success" class="mr-2" to="/login">Login</b-button>
+                    <b-button to="/index copy">Signup</b-button>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar> -->
+    </div>
 </template>
 
 <script>
-    export default {
+  export default {
+    name: "App",
+    data() {
+      return {
+        sideNav: false
+      };
+    },
+    computed: {
+      menuItems() {
+        let menuItems = [
+          { icon: "mdi-face", title: "Sign up", link: "/signup" }, { icon: "mdi-lock-open", title: "Sign in", link: "/signin" }
+        ];
 
+        if (this.userIsAuthenticated) {
+          menuItems = [
+            {
+              icon: "supervisor_account",
+              title: "View Meetups",
+              link: "/meetups"
+            },
+            { icon: "room", title: "Organize Meetup", link: "/meetup/new" },
+            { icon: "person", title: "Profile", link: "/profile" }
+          ];
+        }
+
+        return menuItems;
+      },
+      userIsAuthenticated() {
+        /* return (
+          this.$store.getters.user !== null
+          && this.$store.getters.user !== undefined
+        ); */
+      }
+    },
+    methods: {
+      /* onLogout() {
+        this.$store.dispatch('logout');
+      } */
     }
+  };
 </script>
 
 <style lang="scss" scoped>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,34 +1,33 @@
-
 export default {
-  server: {
-    // port: 3100, // default 3000
-    port: process.env.PORT || 4600,
-  },
-  /*
-  ** Nuxt rendering mode
-  ** See https://nuxtjs.org/api/configuration-mode
-  */
-  mode: 'universal',
-  /*
-  ** Nuxt target
-  ** See https://nuxtjs.org/api/configuration-target
-  */
-  target: 'server',
-  /*
-  ** Headers of the page
-  ** See https://nuxtjs.org/api/configuration-head
-  */
-  head: {
-      title: process.env.npm_package_name || '',
-      meta: [
-          { charset: 'utf-8' },
-          { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-          { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
-          { hid: 'og:title', name: 'og:title', content: 'Spot Brus | Collabarative WhiteBoard'},
-          { hid: 'og:locale', name: 'og:locale', content: 'en_US'},
-          { hid: 'og:url', name: 'og:url', content: process.env.BASE_URL || 'http://localhost:4600'},
-          { hid: 'og:type', name: 'og:type', content: 'website'},
-          { hid: 'og:description', name: 'og:description', content: `Spot Brus | Collabarative WhiteBoard`},
+    server: {
+        // port: 3100, // default 3000
+        port: process.env.PORT || 4600,
+    },
+    /*
+     ** Nuxt rendering mode
+     ** See https://nuxtjs.org/api/configuration-mode
+     */
+    mode: 'universal',
+    /*
+     ** Nuxt target
+     ** See https://nuxtjs.org/api/configuration-target
+     */
+    target: 'server',
+    /*
+     ** Headers of the page
+     ** See https://nuxtjs.org/api/configuration-head
+     */
+    head: {
+        title: process.env.npm_package_name || '',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+            { hid: 'og:title', name: 'og:title', content: 'Spot Brus | Collabarative WhiteBoard' },
+            { hid: 'og:locale', name: 'og:locale', content: 'en_US' },
+            { hid: 'og:url', name: 'og:url', content: process.env.BASE_URL || 'http://localhost:4600' },
+            { hid: 'og:type', name: 'og:type', content: 'website' },
+            { hid: 'og:description', name: 'og:description', content: `Spot Brus | Collabarative WhiteBoard` },
         ],
         link: [
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -36,56 +35,59 @@ export default {
             // { rel: 'stylesheet', type: 'text/css', href: 'https://unpkg.com/bulma-modal-fx/dist/css/modal-fx.min.css' }
         ],
         script: [
-        //   {src: 'https://kit.fontawesome.com/df4e4a88c4.js', crossorigin :'anonymous'},
-          // {src: 'https://unpkg.com/bulma-modal-fx/dist/js/modal-fx.min.js', crossorigin :'anonymous'}
+            //   {src: 'https://kit.fontawesome.com/df4e4a88c4.js', crossorigin :'anonymous'},
+            // {src: 'https://unpkg.com/bulma-modal-fx/dist/js/modal-fx.min.js', crossorigin :'anonymous'}
         ],
-  },
+    },
     /*
-  ** Customize the progress-bar color
-  */
- loading: { color: '#ff6634', throttle: 0 },
-  /*
-  ** Global CSS
-  */
-  css: [
-    '@/assets/scss/main.scss'
-  ],
-  /*
-  ** Plugins to load before mounting the App
-  ** https://nuxtjs.org/guide/plugins
-  */
-  plugins: [
-    {src: '~/plugins/vuelidate'},
-  ],
-  /*
-  ** Auto import components
-  ** See https://nuxtjs.org/api/configuration-components
-  */
-  components: true,
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    // Doc: https://bootstrap-vue.js.org
-    'bootstrap-vue/nuxt',
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
-  ],
-  /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
-  axios: {},
-  /*
-  ** Build configuration
-  ** See https://nuxtjs.org/api/configuration-build/
-  */
-  build: {
-  }
+     ** Customize the progress-bar color
+     */
+    loading: { color: '#ff6634', throttle: 0 },
+    /*
+     ** Global CSS
+     */
+    css: [
+        '@/assets/scss/main.scss'
+    ],
+    /*
+     ** Plugins to load before mounting the App
+     ** https://nuxtjs.org/guide/plugins
+     */
+    plugins: [
+        { src: '~/plugins/vuelidate' },
+        // { src: '~/plugins/vuetify' },
+    ],
+    /*
+     ** Auto import components
+     ** See https://nuxtjs.org/api/configuration-components
+     */
+    components: true,
+    /*
+     ** Nuxt.js dev-modules
+     */
+    buildModules: [
+        ['@nuxtjs/vuetify', {
+            theme: {}
+        }]
+    ],
+    /*
+     ** Nuxt.js modules
+     */
+    modules: [
+        // Doc: https://bootstrap-vue.js.org
+        'bootstrap-vue/nuxt',
+        // Doc: https://axios.nuxtjs.org/usage
+        '@nuxtjs/axios',
+        '@nuxtjs/pwa',
+    ],
+    /*
+     ** Axios module configuration
+     ** See https://axios.nuxtjs.org/options
+     */
+    axios: {},
+    /*
+     ** Build configuration
+     ** See https://nuxtjs.org/api/configuration-build/
+     */
+    build: {}
 }
